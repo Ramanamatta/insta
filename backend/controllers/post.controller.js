2022b2878277275e6d8eee5158b667251cd40038
ramana <ramanamatta2@gmail.com>
2025-08-07 11:32:24 +0530

    dataUri delelted

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,6 +1,5 @@
 import sharp from "sharp";
 import cloudinary from "../utils/cloudinary.js";
-import getDataUri from "../utils/dataUri.js";
 import { Post } from "../models/post.model.js";
 import { User } from "./../models/user.model.js";
 import { Comment } from "../models/coment.model.js";
