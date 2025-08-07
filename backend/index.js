888928235c1112746f1f7173daec8473cce62d28
ramana <ramanamatta2@gmail.com>
2025-08-07 17:56:43 +0530

    origin updated in index.js

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: "https://insta-git-main-ramanamattas-projects.vercel.app",
   credentials: true,
 };  
 app.use(cors(corsOptions));
